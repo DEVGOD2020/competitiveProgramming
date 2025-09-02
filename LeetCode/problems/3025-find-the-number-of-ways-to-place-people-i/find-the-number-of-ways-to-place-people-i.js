be95578a36886f8ee77f002413e255990654736b
DEVGOD2020 <60516202+DEVGOD2020@users.noreply.github.com>
2025-09-01 22:03:53 -0500

    9-1-25

---
+++ LeetCode/problems/3025-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+var numberOfPairs = function(points) {
+    points.sort( 
+        (a,b)=>{
+            return a[0] == b[0] ? b[1]-a[1] : a[0]-b[0]
+        }
+    );
+
+    let score = 0;
+    for(let I = 0; I<points.length; I++){
+        let left = points[I];
+        let maxY = -1;
+        for(let R = I+1; R<points.length; R++){
+            let right = points[R];
+            if( left[0] <= right[0] && left[1] >= right[1]){
+                if(maxY < right[1]){
+                    maxY = Math.max(maxY,right[1]);
+                    score++;
+                }
+            }
+        }
+    }
+
+    return score;
+};
