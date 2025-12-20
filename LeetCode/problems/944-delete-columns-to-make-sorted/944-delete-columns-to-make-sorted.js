8d0777a7f6308b63716d0be16b31176d8e73cbff
DEVGOD2020 <60516202+DEVGOD2020@users.noreply.github.com>
2025-12-19 22:40:22 -0600

    12-19-25

--- LeetCode/problems/944-delete-columns-to-make-sorted/944-delete-columns-to-make-sorted.js
+++ LeetCode/problems/944-delete-columns-to-make-sorted/944-delete-columns-to-make-sorted.js
@@ -4,15 +4,10 @@
  */
 var minDeletionSize = function(strs) {
     let score = 0;
-    for(let col = 0; col<strs[0].length; col++){
-        let prev = strs[0][col];
-        for(let row = 1; row<strs.length; row++){
-            if(prev > strs[row][col]){
-                score++;
-                break;
-            }
-            prev = strs[row][col];
-        }
+    for(let A = 0; A<strs[0].length; A++){
+        score += strs.some( 
+            (el,I)=> I>0 && (strs[I-1][A] > strs[I][A]) 
+        );
     }
     return score;
 };
