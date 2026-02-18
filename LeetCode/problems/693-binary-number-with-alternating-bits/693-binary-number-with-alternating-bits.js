80e39ea09db50914fbb05d28424d46331e50e52e
DEVGOD2020 <60516202+DEVGOD2020@users.noreply.github.com>
2026-02-17 22:31:23 -0600

    Solution: LeetCode Daily 693

--- LeetCode/problems/693-binary-number-with-alternating-bits/693-binary-number-with-alternating-bits.js
+++ LeetCode/problems/693-binary-number-with-alternating-bits/693-binary-number-with-alternating-bits.js
@@ -3,14 +3,6 @@
  * @return {boolean}
  */
 var hasAlternatingBits = function(n) {
-    let prev = n&1;
-    n >>= 1;
-    while(n>0){
-        if( (n&1) === prev){
-            return false;
-        }
-        prev = n&1;
-        n >>= 1;
-    }
-    return true;
+    n = n ^ (n>>1);
+    return (n & (n+1)) == 0;
 };
