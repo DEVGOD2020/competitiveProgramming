92027c6acb7093b1f8a133495e3e8af9160f38a4
DEVGOD2020 <60516202+DEVGOD2020@users.noreply.github.com>
2026-03-07 19:52:15 -0600

    Solution: LeetCode Daily 1980

--- LeetCode/problems/1980-find-unique-binary-string/1980-find-unique-binary-string.js
+++ LeetCode/problems/1980-find-unique-binary-string/1980-find-unique-binary-string.js
@@ -3,29 +3,5 @@
  * @return {string}
  */
 var findDifferentBinaryString = function(nums) {
-    let mySet = new Set( ...[nums.map((el)=>parseInt(el,2))] );
-
-    let state = 0;
-    let backtrack = function(I=0){
-        if(I < nums.length){
-            if(!mySet.has(state)){
-                return true;
-            }
-        }
-        if(I == nums.length){
-            return !mySet.has(state)
-        }
-
-        state ^= (1 << I);
-        if(backtrack(I+1)){return true;}
-
-        state ^= (1 << I);
-        if(backtrack(I+1)){return true;}
-
-        return false;
-    }
-
-    backtrack();
-
-    return state.toString(2).padStart(nums.length,"0");;
+    return nums.map( (el,I)=> +(el[I]!=1)).join("");
 };
