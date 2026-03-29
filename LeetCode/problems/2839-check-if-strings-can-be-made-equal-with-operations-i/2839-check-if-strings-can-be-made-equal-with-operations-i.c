f98a2b538c0cb3294bad82725e516dd0715d3835
DEVGOD2020 <60516202+DEVGOD2020@users.noreply.github.com>
2026-03-29 10:59:55 -0500

    Solution: LeetCode Daily 2839

--- LeetCode/problems/2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.c
+++ LeetCode/problems/2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.c
@@ -1,9 +1,4 @@
-/**
- * @param {string} s1
- * @param {string} s2
- * @return {boolean}
- */
-var canBeEqual = function(s1, s2) {
+bool canBeEqual(char* s1, char* s2) {
     if(s1[1] != s2[1] || s1[3] != s2[3]){
         if(s1[1] != s2[3] || s2[1] != s1[3]){return false;}
     }
@@ -11,4 +6,4 @@ var canBeEqual = function(s1, s2) {
         if(s1[0] != s2[2] || s2[0] != s1[2]){return false;}
     }
     return true;
-};
+}
